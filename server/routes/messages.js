de07170969f9643cadef5aacce982aa3eaa3535e
Anushka Verma <v.anushka786@gmail.com>
2020-03-01 18:48:34 +0530

    add get & delete msg server side functionality

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const router = express.Router({mergeParams: true}); // provides access to the id inside the router
-const { creatMessage } = require("../handlers/messages");
+const router = express.Router({ mergeParams: true }); // provides access to the id inside the router
+const {
+  createMessage,
+  getMessage,
+  deleteMessage
+} = require("../handlers/messages");
 
 // prefix: /api/users/:id/messages
-router.route("/").post(function(req, res){
-    creatMessage
-});
+router.route("/").post(createMessage);
+
+// prefix: /api/users/:id/messages/:message_id
+router
+  .route("/:message_id")
+  .get(getMessage)
+  .delete(deleteMessage);
 
 module.exports = router;
