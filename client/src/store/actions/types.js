6b5c00a9564b17c41938c8c437bc554559ad4eda
Anushka Verma <v.anushka786@gmail.com>
2020-03-03 00:12:00 +0530

    add actions

---
+++ client/src/store/actions/types.js
@@ -0,0 +1,10 @@
+// errors
+export const ADD_ERROR = "ADD_ERROR";
+export const REMOVE_ERROR = "REMOVE_ERROR";
+
+// auth
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+// messages
+export const LOAD_MESSAGES = "LOAD_MESSAGES";
+export const REMOVE_MESSAGE = "REMOVE_MESSAGE";
